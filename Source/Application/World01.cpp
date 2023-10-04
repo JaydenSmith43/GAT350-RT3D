f0980a525576340e60cbcab0f7e1d3af9fd84301
JaydenSmith43 <123499904+JaydenSmith43@users.noreply.github.com>
2023-10-03 23:08:59 -0600

    World01.cpp changes for Hello World assignment

--- Source/Application/World01.cpp
+++ Source/Application/World01.cpp
@@ -7,6 +7,11 @@ namespace nc
 {
     bool World01::Initialize()
     {
+        //for (int i = 0; i < 50; i++)
+        //{
+        //    m_positions.push_back({ randomf(-1, 1), randomf(-1, 1) });
+        //}
+        
         return true;
     }
 
@@ -16,26 +21,71 @@ namespace nc
 
     void World01::Update(float dt)
     {
+        //m_angle += 360 * dt;
+        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -dt : 0;
+        m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? dt : 0;
+
+        m_angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_Q) ? dt * 180 : 0;
+        m_angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_E) ? -dt * 180 : 0;
+
+        m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? dt : 0;
+        m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -dt : 0;
+        m_time += dt;
     }
 
     void World01::Draw(Renderer& renderer)
     {
         // pre-render
         renderer.BeginFrame();
+        for (int i = 0; i < 10; i++)
+        {
+            // render
+            glPushMatrix();
+            glTranslatef(m_position.x, m_position.y, 0);
+            
+            glRotatef(m_angle, 0, 0, 1);
+            //glRotatef(m_angle, 0, 1, 0);
+            //glScalef((sin(m_time * 5) + 1) * 0.5f, 1, 1);
+            glScalef(1, 1, 1);
 
-        // render
-        glBegin(GL_TRIANGLES);
+            glBegin(GL_TRIANGLES);
 
-        glColor3f(1, 0, 0);
-        glVertex2f(-0.5f, -0.5f);
+            glColor3f(1, 0, 0);
+            glVertex2f(-0.5f, -0.5f);
 
-        glColor3f(0, 1, 0);
-        glVertex2f(0, 0.5f);
+            glColor3f(0, 1, 0);
+            glVertex2f(0, 0.5f);
 
-        glColor3f(0, 0, 1);
-        glVertex2f(0.5f, -0.5f);
+            glColor3f(0, 0, 1);
+            glVertex2f(0.5f, -0.5f);
 
-        glEnd();
+            glEnd();
+            glPopMatrix();
+
+
+            glPushMatrix();
+            glTranslatef((sin(m_time * 5)), m_position.y, 0);
+            glScalef((sin(m_time * 5) + 2) / 2, 1, 1);
+
+            glBegin(GL_QUADS);
+
+            glColor3f(1, sin(m_time), 0);
+            glVertex2f(-0.8f, -0.5f);
+
+            glColor3f(1, sin(m_time), 0);
+            glVertex2f(-0.8, -0.2f);
+
+            glColor3f(sin(m_time), 1, 0);
+            glVertex2f(-0.2f, -0.2f);
+
+            glColor3f(sin(m_time), 1, 0);
+            glVertex2f(-0.2f, -0.5f);
+
+            glEnd();
+
+            glPopMatrix();
+        }
+
 
 
         // post-render
